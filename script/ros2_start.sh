aca86ade5d67b8bb37cfc2571ebd764267d79754
nuc-bt <felicita64@gmail.com>
2024-04-05 16:58:56 +0900

    feat : add KEC urdf

--- script/ros2_start.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-echo "Start all ros2 nodes!"
-source /home/nuc-bs/ros2_ws/src/setup.bash && ros2 launch total_launcher total_launch.py
-exit 0
--- script/ros_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "start"
-source /home/nuc-bs/ros2_ws/src/setup.bash&&ros2 launch total_launcher total_launch.py
-exit 0
